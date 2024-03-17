bc3f0fc0a4f4c0f59e9427688f8055ae51e8f665
Sakthikarthick <sakthikarthick3107@gmail.com>
2024-03-17 12:21:25 +0530

    bottom popup edited

--- culinashare_web/src/components/BottomPopup.jsx
+++ culinashare_web/src/components/BottomPopup.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { XCircleIcon } from "@heroicons/react/20/solid";
 const BottomPopup = ({showPopup,message}) => {
-  if (!showPopup) return null;
   return (
-    <div className='fixed bottom-10 z-50 left-10 w-1/3 px-6 py-2 rounded-full backdrop-blur-md 
-                    shadow-md bg-slate-800/70 flex flex-row'>
+    <div className={`fixed bottom-10 z-50 left-10 w-1/3 px-6 py-2 rounded-full backdrop-blur-md 
+                    shadow-md bg-slate-800/70 flex flex-row transition ease-in-out duration-300
+                    ${showPopup ? 'opacity-100 scale-110' : 'opacity-0'}
+                    
+                    `}>
       <p className='flex-1 text-white font-poppins text-lg'>{message}</p>
       {/* <XCircleIcon className="h-6 w-6 text-gray-500" /> */}
     </div>
